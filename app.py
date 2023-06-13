5b8353e738d3088b3b94e23393cf31aac8ab1efd
Insomniac2904 <109868197+Insomniac2904@users.noreply.github.com>
2023-06-13 12:53:35 +0000

    added dynamic data and api endpoint

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'title': 'Backend Engineer',
+  'location': 'Bangalore',
+  'salary': 'Rs. 2,000,000'
+}, {
+  'id': 2,
+  'title': 'Frontend Engineer',
+  'location': 'Bangalore',
+  'salary': 'Rs. 2,000,000'
+}, {
+  'id': 3,
+  'title': 'Data Analyst',
+  'location': 'Bangalore',
+  'salary': 'Rs. 2,000,000'
+}]
+
 @app.route('/')
 def hello_world():
-  return "hello world!"
+  return render_template("home.html",jobs=JOBS)
 
-if(__name__=="__main__"):
-  app.run(host="0.0.0.0",debug=True)
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
+
+
+if (__name__ == "__main__"):
+  app.run(host="0.0.0.0", debug=True)
